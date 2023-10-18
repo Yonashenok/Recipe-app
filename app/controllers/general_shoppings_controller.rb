816647f94a1fa6669b3e5a902e07468592201580
Yonashenok <yonashenok24@gmail.com>
2023-10-18 12:30:57 +0300

    fix: linter error

--- app/controllers/general_shoppings_controller.rb
+++ app/controllers/general_shoppings_controller.rb
@@ -1,14 +1,12 @@
 class GeneralShoppingsController < ApplicationController
-	def index
-		@recipe_food = current_user.recipe_foods.group(:food_id).sum(:quantity)
-		@foods = current_user.foods
-		@shopping = []    
-		@foods.each do |food| 
-			unless @recipe_food.key?(food.id)
-				@shopping << food unless @shopping.include?(food) 
-			end
-	end
-	@total_price =  @shopping.sum { |food| food.price }
-	@total_item = @shopping.count
-	end
+  def index
+    @recipe_food = current_user.recipe_foods.group(:food_id).sum(:quantity)
+    @foods = current_user.foods
+    @shopping = []
+    @foods.each do |food|
+      @shopping << food if !@recipe_food.key?(food.id) && !@shopping.include?(food)
+    end
+    @total_price = @shopping.sum(&:price)
+    @total_item = @shopping.count
+  end
 end
