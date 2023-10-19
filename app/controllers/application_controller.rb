07acbcc1251305c36a413ea1ce2a8e4d3e241972
Yonashenok <yonashenok24@gmail.com>
2023-10-19 09:50:33 +0300

    Fix: linter extra spec checker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, alert: exception.message
   end
 
-
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
